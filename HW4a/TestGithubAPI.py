4f655d62ab05d8ac41798efd215b1e7dace661dd
SeeAnish <amalhot6@stevens.edu>
2022-10-07 21:49:21 -0400

    HW4

--- HW4a/TestGithubAPI.py
+++ HW4a/TestGithubAPI.py
@@ -10,7 +10,8 @@ class TestGithubAPI(unittest.TestCase):
         self.assertEqual(fetchRepos('?'), "Repository name doesn't exist")
     def testGithub2(self):
         expected = ['Repo: CS546_C_Group12_Final_Project  Number of [213 chars]: 6']
-        self.assertEqual(fetchRepos('SeeAnish'), ['Repo: CS546_C_Group12_Final_Project  Number of commits: 30', 'Repo: CS555-Agile-Methods-for-Software-Development  Number of commits: 2', 'Repo: CS_555_C_Atom  Number of commits: 30', 'Repo: SSW567  Number of commits: 20', 'Repo: SSW_567_HW01  Number of commits: 6'])
+        self.assertEqual(fetchRepos('yash171298'), ['Repo: CS-541-Artificial-Intelligence  Number of commits: 23', 'Repo: CS-546-Web-Programming  Number of commits: 12', 'Repo: CS-550  Number of commits: 1', 'Repo: CS-554  Number of commits: 30', 'Repo: CS-554-Web-Programming  Number of commits: 1', 'Repo: CS-562-DBMS2-Project  Number of commits: 2', 'Repo: CS-570-Data-Structures  Number of commits: 1', 'Repo: cs546b_group22_final_project  Number of commits: 30', 'Repo: CS555_A_Project_SpiritualBliss  Number of commits: 30', 
+'Repo: CSS-554-WebProgramming-II  Number of commits: 1', 'Repo: CS_554_Group_Project_The_mutables  Number of commits: 30'])
        
 if __name__ == '__main__':
     print('Running unit tests')
