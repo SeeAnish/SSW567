2ba56ab24caa9f93019640524e268b47ee9e92e6
SeeAnish <amalhot6@stevens.edu>
2022-10-11 03:09:57 -0400

    testing new branch.

--- HW4a/TestGithubAPI.py
+++ HW4a/TestGithubAPI.py
@@ -8,6 +8,7 @@ from getgitinfo import fetchRepos
 class TestGithubAPI(unittest.TestCase):
     def testGithub(self):
         self.assertEqual(fetchRepos('?'), "Repository name doesn't exist")
+        print('mocking')
     def testGithub2(self):
         expected = ['Repo: CS546_C_Group12_Final_Project  Number of [213 chars]: 6']
         self.assertEqual(fetchRepos('yash171298'), ['Repo: CS-541-Artificial-Intelligence  Number of commits: 23', 'Repo: CS-546-Web-Programming  Number of commits: 12', 'Repo: CS-550  Number of commits: 1', 'Repo: CS-554  Number of commits: 30', 'Repo: CS-554-Web-Programming  Number of commits: 1', 'Repo: CS-562-DBMS2-Project  Number of commits: 2', 'Repo: CS-570-Data-Structures  Number of commits: 1', 'Repo: cs546b_group22_final_project  Number of commits: 30', 'Repo: CS555_A_Project_SpiritualBliss  Number of commits: 30', 
